604e1e0cd15eeaf336a654a542bf65cb0bbeef13
tung-10 <nqtung1010@gmail.com>
2026-04-21 01:47:13 +0700

    Update datasets

--- dataset/build.py
+++ dataset/build.py
@@ -39,16 +39,16 @@ def build_pretraining_dataset(args):
 def build_dataset(is_train, test_mode, args):
     if is_train:
         mode = 'train'
-        anno_path = os.path.join(args.data_path, '/mnt/data/quangtungbk/Continuous-Action-Recognition-v2/train_clip_front.csv')
+        anno_path = os.path.join(args.data_path, 'train_clip.csv')
     elif test_mode:
         mode = 'test'
         if args.data_set != "Ego-Gesture":
-            anno_path = os.path.join(args.data_path, 'test_clip_rear.csv')
+            anno_path = os.path.join(args.data_path, 'test_clip.csv')
         else:
-            anno_path = os.path.join(args.data_path, 'test_clip_rear.csv')
+            anno_path = os.path.join(args.data_path, 'test_clip.csv')
     else:
         mode = 'validation'
-        anno_path = os.path.join(args.data_path, 'test_clip_rear.csv')
+        anno_path = os.path.join(args.data_path, 'test_clip.csv')
 
     if args.data_set == 'Kinetics-400':
         if not args.sparse_sample:
@@ -107,6 +107,7 @@ def build_dataset(is_train, test_mode, args):
             new_width=320,
             args=args)
         nb_classes = 20
+        
     elif args.data_set == "UTCDA":
         dataset = VideoClsDataset(
             anno_path=anno_path,
@@ -126,6 +127,26 @@ def build_dataset(is_train, test_mode, args):
             args=args
         )
         nb_classes = 7
+        
+    elif args.data_set == "driver":
+        dataset = VideoClsDataset(
+            anno_path=anno_path,
+            data_root=args.data_root,
+            mode=mode,
+            clip_len=args.num_frames,
+            frame_sample_rate=args.sampling_rate,
+            num_segment=1,
+            test_num_segment=args.test_num_segment,
+            test_num_crop=args.test_num_crop,
+            num_crop=1 if not test_mode else 3,
+            keep_aspect_ratio=True,
+            crop_size=args.input_size,
+            short_side_size=args.short_side_size,
+            new_height=256,
+            new_width=320,
+            args=args
+        )
+        nb_classes = 11
 
     elif args.data_set == "Ego-Gesture":
         dataset = VideoClsDataset(
